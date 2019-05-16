1364d49eb55db632d9c4d83f0f4af9365015bba1
KayLa Thomas <kayla.thomas.1997@gmail.com>
2019-05-16 10:18:49 -0400

    Added the ability to delete a form field.

--- js/formLogic.js
+++ js/formLogic.js
@@ -2,13 +2,25 @@ $( document ).ready(function() {
 
     let formCount = 2
 
+//-----------------------------------------
+//          ADD A FORM FIELD
+//-----------------------------------------
     $("#addField").click(function() {
         formCount++
         $("#thisForm").append(`
             <div class="form-group">
-                <label for="formInput${formCount}">Form Label ${formCount}</label>
+                <label for="formInput${formCount}" id="inputLabel${formCount}" contenteditable="true">Form Label ${formCount}</label> <i class="fa fa-trash mx-2" aria-hidden="true" id="trashcan"></i>
                 <input type="text" class="form-control" id="formInput${formCount}">
             </div>
         `)
+        
+        //-----------------------------------------
+        //          DELETE A FORM FIELD
+        //-----------------------------------------
+        $(".fa-trash").click(function(event){
+            event.preventDefault()
+            formCount--
+            $(this).parent().remove()
+        })
     })
 });
