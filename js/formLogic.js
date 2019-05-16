1cf2174a78c1462cf858a94d5c74bb4ff3ffc5e6
KayLa Thomas <kayla.thomas.1997@gmail.com>
2019-05-16 11:30:30 -0400

    Formatted labels to be borderless.

--- js/formLogic.js
+++ js/formLogic.js
@@ -8,19 +8,26 @@ $( document ).ready(function() {
     $("#addField").click(function() {
         formCount++
         $("#thisForm").append(`
+        <div id="formInput${formCount}">
             <div class="form-group">
-                <label for="formInput${formCount}" id="inputLabel${formCount}" contenteditable="true">Form Label ${formCount}</label> <i class="fa fa-trash mx-2" aria-hidden="true" id="trashcan"></i>
-                <input type="text" class="form-control" id="formInput${formCount}">
+                <input type="text" class="form-control form-control-border" id="InputLabel${formCount}" value="Form Label ${formCount}">
             </div>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" id="emptyFormInput${formCount}">
+                <button class="input-group-append btn btn-danger">
+                    <i class="fa fa-trash mx-2" aria-hidden="true"></i>
+                </button>
+            </div>
+        </div>
         `)
-        
+
         //-----------------------------------------
         //          DELETE A FORM FIELD
         //-----------------------------------------
-        $(".fa-trash").click(function(event){
+        $(".btn-danger").click(function(event){
             event.preventDefault()
             formCount--
-            $(this).parent().remove()
+            $(this).parent().parent().remove()
         })
     })
 });
