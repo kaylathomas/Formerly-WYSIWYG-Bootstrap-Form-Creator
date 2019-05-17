2986e4464143e0790c9b05b857a2cdfff3799f1d
KayLa Thomas <kayla.thomas.1997@gmail.com>
2019-05-17 18:47:22 -0400

    Added ability to change form color.

--- js/formLogic.js
+++ js/formLogic.js
@@ -1,6 +1,4 @@
-$( document ).ready(function() {
-
-    let formCount = 2
+let formCount = 2
 
 //-----------------------------------------
 //          ADD A FORM FIELD
@@ -12,7 +10,7 @@ $( document ).ready(function() {
                 <label for="emptyFormInput${formCount}" contenteditable="true">Form Label ${formCount}</label>
                 <div class="input-group mb-3">
                     <input type="text" class="form-control" id="emptyFormInput${formCount}">
-                    <button class="input-group-append btn btn-danger">
+                    <button class="input-group-append btn delete-button">
                         <i class="fas fa-times"></i>
                     </button>
                 </div>
@@ -22,10 +20,12 @@ $( document ).ready(function() {
         //-----------------------------------------
         //          DELETE A FORM FIELD
         //-----------------------------------------
-        $(".btn-danger").click(function(event){
+        $(".delete-button").click(function(event){
             event.preventDefault()
             $(this).parent().parent().remove()
         })
+        
+
 
 //-----------------------------------------
 //          FORMAT LIVE VIEW
@@ -35,9 +35,58 @@ $( document ).ready(function() {
             $("#hiddenDiv button").remove()
             $("#hiddenDiv label").removeAttr("contenteditable")
             $("#hiddenDiv h3").removeAttr("contenteditable")
+        //Adds a submit button to the end of the form.
+            $("#hiddenDiv #thisForm").append('<button type="submit" class="btn btn-primary float-right">Submit</button>')
             $("#liveCode").val($("#hiddenDiv").html())
+        //Clears the hidden div
             $("#hiddenDiv").empty()
         })
     })
-    // for(var i = 0; i < rooms.results.length; i++){
-});
+//-----------------------------------------
+//            CHANGE FORM COLOR
+//-----------------------------------------
+$("#buttonPrimary").click(function(event){
+    $("#cardBody").toggleClass("bg-primary")
+    $("#cardBody").toggleClass("text-white")
+    $("#buttonPrimary").toggleClass("active")
+})
+
+$("#buttonSecondary").click(function(event){
+    $("#cardBody").toggleClass("bg-secondary")
+    $("#cardBody").toggleClass("text-white")
+    $("#buttonSecondary").toggleClass("active")
+})
+
+$("#buttonSuccess").click(function(event){
+    $("#cardBody").toggleClass("bg-success")
+    $("#cardBody").toggleClass("text-white")
+    $("#buttonSuccess").toggleClass("active")
+})
+
+$("#buttonDanger").click(function(event){
+    $("#cardBody").toggleClass("bg-danger")
+    $("#cardBody").toggleClass("text-white")
+    $("#buttonDanger").toggleClass("active")
+})
+
+$("#buttonWarning").click(function(event){
+    $("#cardBody").toggleClass("bg-warning")
+    $("#cardBody").toggleClass("text-white")
+    $("#buttonWarning").toggleClass("active")
+})
+
+$("#buttonInfo").click(function(event){
+    $("#cardBody").toggleClass("bg-info")
+    $("#cardBody").toggleClass("text-white")
+    $("#buttonInfo").toggleClass("active")
+})
+$("#buttonLight").click(function(event){
+    $("#cardBody").toggleClass("bg-light")
+    $("#buttonLight").toggleClass("active")
+})
+
+$("#buttonDark").click(function(event){
+    $("#cardBody").toggleClass("bg-dark")
+    $("#cardBody").toggleClass("text-white")
+    $("#buttonDark").toggleClass("active")
+})
