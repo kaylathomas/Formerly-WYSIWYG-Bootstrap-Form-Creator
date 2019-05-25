4b19fb449db53362fce3276d951a6fe8f66bf5c6
KayLa Thomas <kayla.thomas.1997@gmail.com>
2019-05-24 21:45:38 -0400

    Added range sliders instead of input buttons.

--- js/addInputs.js
+++ js/addInputs.js
@@ -1,61 +1,128 @@
 let formCount = 4
 
-
 //-----------------------------------------
 //          ADD A FORM FIELD
 //-----------------------------------------
-    $("#addField").on("click", function(){
-        formCount++
-        $("#thisForm").append(`
-            <div class="formInput">
-                <span>
-                    <label for="emptyFormInput${formCount}" contenteditable="true" class="font-weight-bold">Form Label ${formCount}</label>
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" id="emptyFormInput${formCount}">
-                        <button type="button" class="input-group-append btn delete-button">
-                            <i class="fas fa-times"></i>
-                        </button>
-                    </div>
-                </span>
-             </div>
-        `)
-    })
 
-//-----------------------------------------
-//          ADD A DUAL FORM FIELD
-//-----------------------------------------
-$("#addDual").on("click", function(){
+function changeFieldNumber() {
+    let fieldNumberInput = document.getElementById("inputFieldNumber").value
+    document.getElementById("inputFieldRange").value = fieldNumberInput
+}
+
+function changeFieldRange() {
+    let fieldNumberRange = document.getElementById("inputFieldRange").value
+    document.getElementById("inputFieldNumber").value = fieldNumberRange
+}
+
+$("#addField").on("click", function(){
     formCount++
+
+    //ADD ONE FIELD
+    function addOneField() {
     $("#thisForm").append(`
         <div class="formInput">
             <span>
-                <div class="form-row">
-                    <div class="col">
-                        <label for="emptyFormInput${formCount}" contenteditable="true" class="font-weight-bold">Form Label ${formCount}</label>
-                        <div class="input-group mb-3">
-                            <input type="text" class="form-control" id="emptyFormInput${formCount}">
-                        </div>
-                    </div>
-                    <div class="col">
-                        <label for="emptyFormInput${formCount}" contenteditable="true" class="font-weight-bold">Form Label ${formCount}</label>
-                        <div class="input-group mb-3">
-                            <input type="text" class="form-control" id="emptyFormInput${formCount}">
-                            <button type="button" class="input-group-append btn delete-button">
-                                <i class="fas fa-times"></i>
-                            </button>
-                        </div>
-                    </div>
+                <label for="emptyFormInput${formCount}" contenteditable="true" class="font-weight-bold">Form Label ${formCount}</label>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" id="emptyFormInput${formCount}">
+                    <button type="button" class="input-group-append btn delete-button">
+                        <i class="fas fa-times"></i>
+                    </button>
                 </div>
             </span>
-        </div>
+         </div>
     `)
-})
+    }
 
+    //ADD TWO FIELDS
+    function addTwoFields() {
+        $("#thisForm").append(`
+            <div class="formInput">
+                <span>
+                    <div class="form-row">
+                        <div class="col">
+                            <label for="emptyFormInput${formCount}" contenteditable="true" class="font-weight-bold">Form Label ${formCount}</label>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" id="emptyFormInput${formCount}">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <label for="emptyFormInput${formCount}" contenteditable="true" class="font-weight-bold">Form Label ${formCount}</label>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" id="emptyFormInput${formCount}">
+                                <button type="button" class="input-group-append btn delete-button">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </span>
+            </div>
+        `)
+        }
+    
+    //ADD THREE FIELDS
+    function addThreeFields() {
+        $("#thisForm").append(`
+            <div class="formInput">
+                <span>
+                    <div class="form-row">
+                        <div class="col">
+                            <label for="emptyFormInput${formCount}" contenteditable="true" class="font-weight-bold">Form Label ${formCount}</label>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" id="emptyFormInput${formCount}">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <label for="emptyFormInput${formCount}" contenteditable="true" class="font-weight-bold">Form Label ${formCount}</label>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" id="emptyFormInput${formCount}">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <label for="emptyFormInput${formCount}" contenteditable="true" class="font-weight-bold">Form Label ${formCount}</label>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" id="emptyFormInput${formCount}">
+                                <button type="button" class="input-group-append btn delete-button">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </span>
+            </div>
+        `)
+        }
+    
+    //INPUT CONTROL FLOW
+    if ($("#inputFieldNumber").val() == 1) {
+        addOneField()
+    } else if ($("#inputFieldNumber").val() == 2){
+        addTwoFields()
+    } else if ($("#inputFieldNumber").val() == 3){
+        addThreeFields()
+    }
+})
+   
 //-----------------------------------------
 //              ADD A CHECKBOX
 //-----------------------------------------
+
+function changeCheckboxNumber() {
+    let checkboxNumberInput = document.getElementById("inputCheckboxNumber").value
+    document.getElementById("inputCheckboxRange").value = checkboxNumberInput
+}
+
+function changeCheckboxRange() {
+    let checkboxNumberRange = document.getElementById("inputCheckboxRange").value
+    document.getElementById("inputCheckboxNumber").value = checkboxNumberRange
+}
+
 $("#addCheckbox").on("click", function(){
     formCount++
+
+    //ADD ONE CHECKBOX
+    function addOneCheckbox(){
     $("#thisForm").append(`
         <div class="formInput">
             <span>
@@ -72,14 +139,11 @@ $("#addCheckbox").on("click", function(){
             </span>
         </div>
     `)
-})
+    }
 
-//-----------------------------------------
-//          ADD A DUAL CHECKBOX
-//-----------------------------------------
-$("#dualCheckboxes").on("click", function(){
-    formCount++
-    $("#thisForm").append(`
+    //ADD TWO CHECKBOXES
+    function addTwoCheckboxes(){
+        $("#thisForm").append(`
         <div class="formInput">
             <span>
                 <div class="form-row">
@@ -103,14 +167,11 @@ $("#dualCheckboxes").on("click", function(){
             </span>
         </div>
     `)
-})
+    }
 
-//-----------------------------------------
-//          ADD A TRIPLE CHECKBOX
-//-----------------------------------------
-$("#tripleCheckboxes").on("click", function(){
-    formCount++
-    $("#thisForm").append(`
+    //ADD THREE CHECKBOXES
+    function addThreeCheckboxes(){
+        $("#thisForm").append(`
         <div class="formInput">
             <span>
                 <div class="form-row">
@@ -142,13 +203,38 @@ $("#tripleCheckboxes").on("click", function(){
             </span>
         </div>
     `)
+    }
+
+    //CHECKBOX CONTROL FLOW
+    if ($("#inputCheckboxNumber").val() == 1) {
+        addOneCheckbox()
+    } else if ($("#inputCheckboxNumber").val() == 2){
+        addTwoCheckboxes()
+    } else if ($("#inputCheckboxNumber").val() == 3){
+        addThreeCheckboxes()
+    }
 })
+
 
 //-----------------------------------------------
 //              ADD A RADIO BUTTON
 //-----------------------------------------------
+
+function changeRadioNumber() {
+    let radioNumberInput = document.getElementById("inputRadioNumber").value
+    document.getElementById("inputRadioRange").value = radioNumberInput
+}
+
+function changeRadioRange() {
+    let radioNumberRange = document.getElementById("inputRadioRange").value
+    document.getElementById("inputRadioNumber").value = radioNumberRange
+}
+
 $("#addRadio").on("click", function(){
     formCount++
+
+    //ADD ONE RADIO BUTTON
+    function addOneRadio(){
     $("#thisForm").append(`
         <div class="formInput">
             <span>
@@ -165,13 +251,10 @@ $("#addRadio").on("click", function(){
             </span>
         </div>
     `)
-})
+    }
 
-//-------------------------------------------------
-//              ADD DUAL RADIO BUTTONS
-//-------------------------------------------------
-$("#dualRadio").on("click", function(){
-    formCount++
+    //ADD TWO RADIO BUTTONS
+    function addTwoRadios(){
     $("#thisForm").append(`
         <div class="formInput">
             <span>
@@ -196,13 +279,10 @@ $("#dualRadio").on("click", function(){
             </span>
         </div>
     `)
-})
+    }
 
-//---------------------------------------------
-//          ADD TRIPLE RADIO BUTTONS
-//---------------------------------------------
-$("#tripleRadio").on("click", function(){
-    formCount++
+    //ADD THREE RADIO BUTTONS
+    function addThreeRadios(){
     $("#thisForm").append(`
         <div class="formInput">
             <span>
@@ -235,6 +315,16 @@ $("#tripleRadio").on("click", function(){
             </span>
         </div>
     `)
+    }
+
+    //CHECKBOX CONTROL FLOW
+    if ($("#inputRadioNumber").val() == 1) {
+        addOneRadio()
+    } else if ($("#inputRadioNumber").val() == 2){
+        addTwoRadios()
+    } else if ($("#inputRadioNumber").val() == 3){
+        addThreeRadios()
+    }
 })
 
 //------------------------------------
