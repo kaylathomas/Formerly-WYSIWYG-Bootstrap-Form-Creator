c56ed3f7808f2333006c0b7d83e9a42de4915eb1
KayLa Thomas <kayla.thomas.1997@gmail.com>
2019-06-05 19:32:04 -0400

    The app is functional! No backend yet.

--- js/formChanges.js
+++ js/formChanges.js
@@ -1,12 +1,20 @@
 //-----------------------------------------
 //          FORMAT LIVE VIEW
 //-----------------------------------------
-$("#getHTML").on("click", function () {
+
+$("#getHTML").on("click", function (event) {
+    event.preventDefault(event)
 //Removes all delete buttons and contenteditable functionality
     $("#hiddenDiv").append($("#yourForm").html())
     $("#hiddenDiv button").remove()
     $("#hiddenDiv label").removeAttr("contenteditable")
     $("#hiddenDiv h3").removeAttr("contenteditable")
+    $("#hiddenDiv small").remove()
+    $("#hiddenDiv br").remove()
+    $("#hiddenDiv .fa-pen").remove()
+    $("#hiddenDiv .input-group").addClass("mb-2")
+    $("#hiddenDiv .formInput").removeClass("my-2")
+    $("#hiddenDiv .move-button").remove()
 //Adds a submit button to the end of the form.
 //Button color depends on color selected for form.
     if ($("#buttonPrimary").hasClass("active") || $("#buttonSecondary").hasClass("active") || $("#buttonSuccess").hasClass("active") || $("#buttonDanger").hasClass("active") || $("#buttonWarning").hasClass("active") || $("#buttonInfo").hasClass("active") || $("#buttonDark").hasClass("active")) {
@@ -18,9 +26,65 @@ $("#getHTML").on("click", function () {
 //Regex to delete all blank lines
     let liveCodeValue = $('#liveCode').val();
     let newVal = liveCodeValue.replace(/^\s*[\r\n]/gm, '');
-    $('#liveCode').val(newVal);
+    $('#liveCode').val(newVal)
+    $('.preview-container').html(newVal)
     //Clears the hidden div
     $("#hiddenDiv").empty()
+})
+
+//-----------------------------------------
+//              LABELS UTILITY
+//-----------------------------------------
+//-----------------------
+//      FONT WEIGHT
+//-----------------------
+    $("#fontWeightLightBtn").click(function(){
+        $(".font-weight-button").removeClass("active")
+        $(this).addClass("active")
+        $("label").removeClass("font-weight-bold")
+        $("label").addClass("font-weight-light")
+    })
+
+    $("#fontWeightNormalBtn").click(function(){
+        $(".font-weight-button").removeClass("active")
+        $(this).addClass("active")
+        $("label").removeClass("font-weight-bold")
+        $("label").removeClass("font-weight-light")
+    })
+
+    $("#fontWeightBoldBtn").click(function(){
+        $(".font-weight-button").removeClass("active")
+        $(this).addClass("active")
+        $("label").addClass("font-weight-bold")
+        $("label").removeClass("font-weight-light")
+    })
+
+//-----------------------
+//      TEXT ALIGN
+//-----------------------
+    
+$("#textAlignLeftBtn").click(function(){
+    $(".text-align-button").removeClass("active")
+    $(this).addClass("active")
+    // $("label").removeClass("mx-auto")
+    $("label").removeClass("float-right")
+    $("label").addClass("float-left")
+})
+
+// $("#textAlignCenterBtn").click(function(){
+//     $(".text-align-button").removeClass("active")
+//     $(this).addClass("active")
+//     $("label").removeClass("float-left")
+//     $("label").removeClass("float-right")
+//     $("label").addClass("mx-auto")
+// })
+
+$("#textAlignRightBtn").click(function(){
+    $(".text-align-button").removeClass("active")
+    $(this).addClass("active")
+    $("label").removeClass("float-left")
+    // $("label").removeClass("mx-auto")
+    $("label").addClass("float-right")
 })
 
 //-----------------------------------------
@@ -80,6 +144,7 @@ $("#buttonInfo").click(function(event){
     $("#cardBody").toggleClass("text-white")
     $("#buttonInfo").toggleClass("active")
 })
+
 $("#buttonLight").click(function(event){
     $(".color-button").removeClass("active")
     $("#cardBody").removeClass("bg-secondary bg-success bg-danger bg-warning bg-info bg-light bg-dark text-white")
@@ -96,3 +161,14 @@ $("#buttonDark").click(function(event){
     $("#cardBody").toggleClass("text-white")
     $("#buttonDark").toggleClass("active")
 })
+
+//CUSTOM FORM COLOR CHANGE:
+$("#customFormBackground").change(function(){
+    var colorValue = $(this).val();
+    $("thisForm").css("background-color", `${colorValue}`);
+});
+
+//-----------------------------------------
+//          DRAG & DROP FEATURE
+//-----------------------------------------
+
---
+++ js/navbarScript.js
@@ -0,0 +1,93 @@
+//============================
+//		NAVBAR DISPLAY
+//============================
+
+let menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+		menuRight = document.getElementById( 'cbp-spmenu-s2' ),
+		showLeft = document.getElementById( 'showLeft' ),
+		showRight = document.getElementById( 'showRight' ),
+		showLeftPush = document.getElementById( 'showLeftPush' ),
+		showRightPush = document.getElementById( 'showRightPush' ),
+		body = document.body;
+
+$(".showLeft").click(function() {
+	classie.toggle( this, 'active' )
+	classie.toggle( menuLeft, 'cbp-spmenu-open' )
+	disableOther( 'showLeft' )
+})
+
+$("#showRight").click(function() {
+	classie.toggle( this, 'active' );
+	classie.toggle( menuRight, 'cbp-spmenu-open' );
+	disableOther( 'showRight' );
+})
+
+$("#showLeftPush").click(function() {
+	classie.toggle( this, 'active' );
+	classie.toggle( body, 'cbp-spmenu-push-toright' );
+	classie.toggle( menuLeft, 'cbp-spmenu-open' );
+	disableOther( 'showLeftPush' );
+})
+
+$("#showRightPush").click(function() {
+	classie.toggle( this, 'active' );
+	classie.toggle( body, 'cbp-spmenu-push-toleft' );
+	classie.toggle( menuRight, 'cbp-spmenu-open' );
+	disableOther( 'showRightPush' );
+});
+
+function disableOther( button ) {
+	if( button !== 'showLeft' ) {
+		classie.toggle( showLeft, 'disabled' );
+	}
+	if( button !== 'showRight' ) {
+		classie.toggle( showRight, 'disabled' );
+	}
+	if( button !== 'showLeftPush' ) {
+		classie.toggle( showLeftPush, 'disabled' );
+	}
+	if( button !== 'showRightPush' ) {
+		classie.toggle( showRightPush, 'disabled' );
+	}
+}
+
+//============================
+//    DEVELOPER MODE LOGIC
+//============================
+
+$("#simpleEditor").click(function(){
+	$("#developerMode").removeClass("active")
+	$(this).addClass("active")
+	$("#yourForm .input-group").addClass("mb-2")
+
+	$("#yourForm .edit-attributes").addClass("d-none")
+})
+
+$("#developerMode").click(function(){
+	$("#simpleEditor").removeClass("active")
+	$(this).addClass("active")
+	$("#yourForm .input-group").removeClass("mb-2")
+
+	$("#yourForm .edit-attributes").removeClass("d-none")
+})
+
+$("#whiteLogo").hide()
+
+//============================
+//		NIGHTMODE LOGIC
+//============================
+
+$("#nightModeButton").on('change.bootstrapSwitch', function(e) {
+	e.preventDefault(e)
+	$("body").toggleClass("bg-black")
+	$(".accordion").toggleClass("border-black border-white side-nav-shadow")
+	$(".py-0").toggleClass("bg-dark-gray bg-black")
+	$(".accordion .font-weight-lighter").toggleClass("text-white")
+	$(".accordion .font-weight-light").toggleClass("text-white")
+	$(".nav-borders").toggleClass("border-gray border-white")
+	$("#buttonCustom").toggleClass("btn-outline-dark btn-outline-light")
+	$(".font-weight-button").toggleClass("btn-outline-light btn-outline-secondary")
+	$(".text-align-button").toggleClass("btn-outline-light btn-outline-secondary")
+	$("#blackLogo").toggle()
+	$("#whiteLogo").toggle()
+});
